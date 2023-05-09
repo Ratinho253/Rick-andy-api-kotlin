176b543c0491d4ed55119adc75bfd5732b58fb2c
JOÃO VICTOR DA SILVA <22282229@dsn127m1242376.cfp127aluno.local>
2023-05-09 16:52:52 -0300

    terminei

--- app/src/main/java/br/senai/sp/jandira/rickandmorty/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/rickandmorty/MainActivity.kt
@@ -1,25 +1,30 @@
 package br.senai.sp.jandira.rickandmorty
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.rickandmorty.model.Info
+import br.senai.sp.jandira.rickandmorty.model.Origin
 import br.senai.sp.jandira.rickandmorty.model.characterList
 import br.senai.sp.jandira.rickandmorty.service.RetrofitFactory
 import br.senai.sp.jandira.rickandmorty.ui.theme.RickAndMortyTheme
+import coil.compose.AsyncImage
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -47,12 +52,15 @@ fun Greeting(name: String) {
     var listCharacter by remember {
         mutableStateOf(listOf<br.senai.sp.jandira.rickandmorty.model.Character>())
     }
+    var info by remember {
+        mutableStateOf(Info())
+    }
 
-    Column() {
+    Column(modifier = Modifier.padding(16.dp)) {
         Button(onClick = {
 
             // cria uma chamada para o endpoint
-            val call = RetrofitFactory().getCharacterService().getCharacter()
+            val call = RetrofitFactory().getCharacterService().getCharacterByNameAndStatus(name = "Rick", status = "")
 
             // Executar a chamada
             call.enqueue(object : Callback<characterList >{
@@ -61,16 +69,26 @@ fun Greeting(name: String) {
                     response: Response<characterList>
                 ) {
                     listCharacter =  response.body()!!.results
+                    info = response.body()!!.info
                 }
 
                 override fun onFailure(call: Call<characterList>, t: Throwable) {
-
+                    Log.i("Ds2-T", "onFailure: ${t.message}")
                 }
 
             })
 
         }) {
             Text(text = "Listar personagens")
+        }
+        Row() {
+            Text(text = "Count", modifier = Modifier.size(width = 60.dp, height = 20.dp))
+
+            Text(text = "${info.count}", modifier = Modifier.size(width = 40.dp, height = 20.dp), textAlign = TextAlign.End)
+        }
+        Row() {
+            Text(text = "pages ", modifier = Modifier.size(width = 60.dp, height = 20.dp))
+            Text(text = "${info.pages}", modifier = Modifier.size(width = 40.dp, height = 20.dp), textAlign = TextAlign.End)
         }
         LazyColumn(){
             items(listCharacter){
@@ -79,19 +97,23 @@ fun Greeting(name: String) {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(
-                            horizontal = 16.dp,
+                            horizontal = 0.dp,
                             vertical = 4.dp
                         )
                 ) {
-                    Column(modifier = Modifier.padding(8.dp)) {
-                        Text(
-                            text = it.name,
-                            fontSize = 22.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                        Text(text = it.species)
+                    Row(modifier = Modifier.padding(8.dp)) {
+                        AsyncImage(model = it.image, contentDescription = "Character avatar", modifier = Modifier.clip(shape = CircleShape))
+
+                        Column(modifier = Modifier.padding(8.dp)) {
+                            Text(
+                                text = it.name,
+                                fontSize = 22.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(text = it.species)
+                            Text(text = it.origin.name)
+                        }
                     }
-                    
                 }
             }
         }
--- app/src/main/java/br/senai/sp/jandira/rickandmorty/model/characterList.kt
+++ app/src/main/java/br/senai/sp/jandira/rickandmorty/model/characterList.kt
@@ -1,5 +1,8 @@
 package br.senai.sp.jandira.rickandmorty.model
 
 data class characterList(
+
+    val info: Info,
     val results : List<br.senai.sp.jandira.rickandmorty.model.Character>
+
 )
